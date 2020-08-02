d07189a88a7ba5b4faea8e50e498873fd95ef823
AkashKhamkar <34622497+AkashKhamkar@users.noreply.github.com>
2020-08-02 14:18:04 +0530

    adding upsample and downsample to generator

--- Model/Generator.py
+++ Model/Generator.py
@@ -1,3 +1,50 @@
+def downsample(filters, size, apply_batchnorm=True):
+  initializer = tf.random_normal_initializer(0., 0.02)
+
+  result = tf.keras.Sequential()
+  result.add(
+      tf.keras.layers.Conv2D(filters, size, strides=2, padding='same',
+                             kernel_initializer=initializer, use_bias=False))
+
+  if apply_batchnorm:
+    result.add(tf.keras.layers.BatchNormalization(momentum=0.8))
+
+  result.add(tf.keras.layers.LeakyReLU(alpha=0.2))
+
+  return result
+
+down_model = downsample(3, 4)
+down_result = down_model(tf.expand_dims(inp, 0))
+print("The downsample result shape is :")
+print (down_result.shape)
+
+def upsample(filters, size, apply_dropout=False):
+  initializer = tf.random_normal_initializer(0., 0.02)
+
+  result = tf.keras.Sequential()
+  result.add(
+    tf.keras.layers.Conv2DTranspose(filters, size, strides=2,
+                                    padding='same',
+                                    kernel_initializer=initializer,
+                                    use_bias=False))
+
+  result.add(tf.keras.layers.BatchNormalization(momentum=0.8))
+
+  if apply_dropout:
+      result.add(tf.keras.layers.Dropout(0.5))
+
+  result.add(tf.keras.layers.ReLU())
+
+  return result
+
+up_model = upsample(3, 4)
+up_result = up_model(down_result)
+print("The upsample result shape is :")
+print (up_result.shape)
+
+
+
+
 class Generator_(tf.keras.Model):
 
     def __init__(self,):
